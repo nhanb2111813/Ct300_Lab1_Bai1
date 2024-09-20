06cc33b3ee068a9c81f4f74ee08b6490cb63eb30
Hoang Nhan <nhanb2111813@gmail.com>
2024-09-20 16:55:15 +0700

    Them chuong trinh in tu 1 den 10

---
+++ PrintNumbers.java
@@ -0,0 +1,9 @@
+package Ct300_Lab1_Bai1;
+public class PrintNumbers {
+    public static void main(String[] args) {
+        // Sử dụng vòng lặp for để in từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
